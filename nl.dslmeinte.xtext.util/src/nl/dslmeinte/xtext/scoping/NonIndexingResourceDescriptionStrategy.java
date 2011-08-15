0fddaa329f07e7ad7449f6e67eababa00a6aa0f6
Meinte Boersma <meinte.boersma@gmail.com>
2011-08-15 23:12:32 +0200

    changed class name and added some Javadoc

--- nl.dslmeinte.xtext.util/src/nl/dslmeinte/xtext/scoping/NonIndexingResourceDescriptionStrategy.java
+++ nl.dslmeinte.xtext.util/src/nl/dslmeinte/xtext/scoping/NonIndexingResourceDescriptionStrategy.java
@@ -6,16 +6,20 @@ import org.eclipse.xtext.resource.IDefaultResourceDescriptionStrategy;
 import org.eclipse.xtext.resource.IEObjectDescription;
 import org.eclipse.xtext.resource.IReferenceDescription;
 import org.eclipse.xtext.util.IAcceptor;
+import org.eclipse.xtext.validation.NamesAreUniqueValidator;
 
 /**
  * Implementation of {@link IDefaultResourceDescriptionStrategy} which exports
  * nothing from any resource is indexed(/exported). This effectively disables
  * all cross-referencing across file boundary among a few other aspects which
- * rely on the names of exported objects, like unique names validation.
+ * rely on the names of exported objects, like unique names validation courtesy
+ * of the {@link NamesAreUniqueValidator} composed check. This is useful in case
+ * all your resources really do not depend on other resources and you do not
+ * need the default names-are-unique validation.
  * 
  * @author Meinte Boersma
  */
-public class NonIndexingResourceDescription implements IDefaultResourceDescriptionStrategy {
+public class NonIndexingResourceDescriptionStrategy implements IDefaultResourceDescriptionStrategy {
 
 	@Override
 	public boolean createEObjectDescriptions(EObject eObject, IAcceptor<IEObjectDescription> acceptor) {
